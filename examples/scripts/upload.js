40e57e2b5b798f415ffc2398a54d7dba5da5f333
Connor Adams <connor.adams@uoit.net>
2022-05-24 22:49:07 -0400

    changed s3 upload to local download

--- examples/scripts/upload.js
+++ examples/scripts/upload.js
@@ -12,10 +12,6 @@
    */
   var original;
 
-  /**
-   * ids for aws related inputs
-   */
-  var awsFields = ['accessKeyId', 'policy', 'signature', 'bucket'];
 
   /**
    * Enable fields identified by ids
@@ -24,16 +20,6 @@
     ids.forEach(function(id) {
       document.getElementById(id).removeAttribute('disabled');
     })
-  }
-
-  /**
-   * Determine if inputs identified by ids have values
-   */
-  function inputsComplete(ids) {
-    return ids.every(function(id) {
-      var val = document.getElementById(id);
-      return !!val.value;
-    });
   }
 
   /**
@@ -76,12 +62,8 @@
       .image(watermark.image[position](0.5))
       .then(function(marked) {
 
-
-
         resetPreviewImage();
         document.getElementById('preview').appendChild(marked);
-
-        enableFields(awsFields);
       });
   }
 
@@ -106,63 +88,22 @@
     return !!watermark.value;
   }
 
-  /**
-   * Get a FormData object ready for uploading to S3
-   */
-  function getFormData(blob, filename, accessKeyId, policy, signature) {
-    var fd = new FormData(),
-        params = {
-          key: filename,
-          AWSAccessKeyId: accessKeyId,
-          acl: 'private',
-          policy: policy,
-          signature: signature,
-          'Content-Type': '$Content-Type',
-          file: [blob, 'watermark.png']
-        };
-
-    for (var k in params) {
-      var args = Array.isArray(params[k]) ? params[k] : [params[k]];
-      fd.append.apply(fd, [k].concat(args));
-    }
-
-    return fd;
-  }
-
-  /**
-   * Perform the upload.
-   *
-   * @param {FormData}
-   * @param {Function} progress handler
-   * @param {Function} completion handler
-   * @param {Function} error handler
-   */
   function upload(onProgress, onComplete, onError) {
-    var req = new XMLHttpRequest(),
-        key = "watermark-" + Date.now().toString() + '.png',
-        img = document.querySelector('#preview img'),
-        keyId = document.getElementById('accessKeyId'),
-        policy = document.getElementById('policy'),
-        signature = document.getElementById('signature'),
-        bucket = document.getElementById('bucket');
+
+    var img = document.querySelector('#preview img');
 
     watermark([img])
       .blob(function(target) { return target; })
       .then(function(blob) {
-        var fd = getFormData(blob, key, keyId.value, policy.value, signature.value);
-        req.open('POST', 'https://' + bucket.value + '.s3.amazonaws.com/', true);
-        req.upload.onprogress = onProgress;
-        req.onreadystatechange = function() {
-          if (req.readyState === 4) {
-            if (! /Error/.test(req.responseText)) { //simple test for AWS error response
-              onComplete();
-            } else {
-              onError(req.responseText);
-            }
-          }
-        }
-        req.addEventListener('error', onError, false);
-        req.send(fd);
+
+        const imageURL = URL.createObjectURL(blob)
+        const link = document.createElement('a')
+        link.href = imageURL
+        link.download = 'watermarkedImage'
+        document.body.appendChild(link)
+        link.click()
+        document.body.removeChild(link)
+
       });
   }
 
@@ -187,17 +128,6 @@
       }
     });
 
-    /**
-     * On keyup for aws inputs, check if the others have been filled in. Once all
-     * have been filled in, enable the upload button
-     */
-    awsFields.forEach(function (id) {
-      document.getElementById(id).addEventListener('keyup', function () {
-        if (inputsComplete(awsFields)) {
-          enableFields(['upload']);
-        }
-      });
-    });
 
     /**
      * Handle form submission - i.e actually do the upload
