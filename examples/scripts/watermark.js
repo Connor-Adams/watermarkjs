19d0d555e32def0a156d808d2f6849097dba76f4
Connor Adams <connor.adams@uoit.net>
2022-06-02 20:34:13 -0400

    fixed everything

--- examples/scripts/watermark.js
+++ examples/scripts/watermark.js
@@ -282,9 +282,17 @@ function createImage(url, onload) {
 
 function drawImage(img, canvas) {
   var ctx = canvas.getContext('2d');
-  canvas.width = img.width;
-  canvas.height = img.height;
-  ctx.drawImage(img, 0, 0);
+  canvas.width = 1080;
+  canvas.height = 1080;
+  var canvas = ctx.canvas ;
+  var hRatio = canvas.width  / img.width    ;
+  var vRatio =  canvas.height / img.height  ;
+  var ratio  = Math.max ( hRatio, vRatio );
+  var centerShift_x = ( canvas.width - img.width*ratio ) / 2;
+  var centerShift_y = ( canvas.height - img.height*ratio ) / 2;  
+  ctx.clearRect(0,0,canvas.width, canvas.height);
+  ctx.drawImage(img, 0, 0, img.width, img.height,
+    centerShift_x,centerShift_y,img.width*ratio, img.height*ratio);
   return canvas;
 }
 /**
